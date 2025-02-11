f17b331ef9a8fcc9d947434216d66e744860e0b7
YudaClairee <djibyuda@gmail.com>
2025-02-11 15:23:01 +0700

    soal 3 dan 4 kebalik

--- quiz/part3/soal3.js
+++ quiz/part3/soal3.js
@@ -1,12 +1,14 @@
-function tentukanDeretGeometri(arr) {
-  if(arr.length <= 2) {return true;}
-  let selisih = arr[1] - arr[0];
-  for(let i = 0; i < arr.length - 1; i++) {
-    if(arr[i + 1] - arr[i] !== selisih) {
-      return false;
-    }  
-  }
-  return true;
-  };
-  
-  console.log(tentukanDeretGeometri([1, 2, 3, 4, 5]));
+
+
+  function tentukanDeretGeometri(arr) {
+    if(arr.length <= 2) {return true;}
+    let selisih = arr[1] / arr[0];
+    for(let i = 0; i < arr.length - 1; i++) {
+      if(arr[i + 1] / arr[i] !== selisih) {
+        return false;
+      }  
+    }
+    return true;
+    };
+    
+    console.log(tentukanDeretGeometri([1, 3, 9, 27, 81]));
--- quiz/part3/soal4.js
+++ quiz/part3/soal4.js
@@ -1,12 +1,12 @@
-function tentukanDeretAritmatika(arr) {
+function tentukanDeret(arr) {
   if(arr.length <= 2) {return true;}
-  let selisih = arr[1] / arr[0];
+  let selisih = arr[1] - arr[0];
   for(let i = 0; i < arr.length - 1; i++) {
-    if(arr[i + 1] / arr[i] !== selisih) {
+    if(arr[i + 1] - arr[i] !== selisih) {
       return false;
     }  
   }
   return true;
   };
   
-  console.log(tentukanDeretAritmatika([1, 3, 9, 27, 81]));
+  console.log(tentukanDeretGeometri([1, 2, 3, 4, 5]));
