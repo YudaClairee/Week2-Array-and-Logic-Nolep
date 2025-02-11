0ba9c4d36d84ba92470c19d5f6ed59976b8cbed1
YudaClairee <djibyuda@gmail.com>
2025-02-11 15:36:27 +0700

    benerin no 3 dan 4 yang kebalik

--- quiz/part3/soal3.js
+++ quiz/part3/soal3.js
@@ -1,14 +1,18 @@
+function tentukanDeretAritmatika(arr) {
+  if(arr.length <= 2) {return true;}
+  let selisih = arr[1] - arr[0];
+  for(let i = 0; i < arr.length - 1; i++) {
+    if(arr[i + 1] - arr[i] !== selisih) {
+      return false;
+    }  
+  }
+  return true;
+  };
 
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // fals
 
-  function tentukanDeretGeometri(arr) {
-    if(arr.length <= 2) {return true;}
-    let selisih = arr[1] / arr[0];
-    for(let i = 0; i < arr.length - 1; i++) {
-      if(arr[i + 1] / arr[i] !== selisih) {
-        return false;
-      }  
-    }
-    return true;
-    };
-    
-    console.log(tentukanDeretGeometri([1, 3, 9, 27, 81]));
+  
--- quiz/part3/soal4.js
+++ quiz/part3/soal4.js
@@ -1,12 +1,16 @@
-function tentukanDeret(arr) {
+function tentukanDeretGeometri(arr) {
   if(arr.length <= 2) {return true;}
-  let selisih = arr[1] - arr[0];
+  let selisih = arr[1] / arr[0];
   for(let i = 0; i < arr.length - 1; i++) {
-    if(arr[i + 1] - arr[i] !== selisih) {
+    if(arr[i + 1] / arr[i] !== selisih) {
       return false;
     }  
   }
   return true;
   };
   
-  console.log(tentukanDeretGeometri([1, 2, 3, 4, 5]));
+  console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+  console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+  console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+  console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+  console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
